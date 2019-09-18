26382ce64b393ad000f84ee386bbc839d2ca6c7f
Niklas Mäki <niklas.maki@gmail.com>
2019-09-18 17:45:47 +0300

    Installed Flask.

---
+++ src/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_page():
+    return render_template("index.html")
+
+
+@app.route('/api/route')
+def get_route():
+    return "Route data here"
+
+if __name__ == '__main__':
+    app.run(debug=True)
