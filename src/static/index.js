b1b9e0b6e2e68a8a29958309e2a3d176078b5684
Niklas Mäki <niklas.maki@gmail.com>
2019-09-27 18:45:20 +0300

    Autocomplete finished

--- src/static/index.js
+++ src/static/index.js
@@ -1,12 +1,31 @@
 
 
 function getRoute() {
-    startAddress = $('#startAddress').val()
-    endAddress = $('#startAddress').val()
-    
-    $.get('/api/route', {startAddress, endAddress})
-        .done(function(data) {
+    var startAddress = $('#startAddress').val()
+    var endAddress = $('#startAddress').val()
+
+    $.get('/api/route', { startAddress, endAddress })
+        .done(data => {
             console.log(data)
         })
     return false
 }
+
+function autoComplete(request, response) {
+    $.get('http://api.digitransit.fi/geocoding/v1/autocomplete', { text: request.term, sources: 'osm' })
+        .done(data => {
+            console.log(data)
+            response(data.features
+                .filter(feature => feature.properties.region === 'Uusimaa')
+                .map(feature => feature.properties.label))
+        })
+}
+
+$(document).ready(() => {
+    $('#startAddress').autocomplete({
+        source: autoComplete
+    })
+    $('#endAddress').autocomplete({
+        source: autoComplete
+    })
+})
