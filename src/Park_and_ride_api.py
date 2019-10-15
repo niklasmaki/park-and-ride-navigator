bf41869621cebf7274b86de1844c2c83e188c8a8
Thierry Botty <thierry.botty@helsinki.fi>
2019-10-15 14:14:21 +0300

    optimized the park and ride data search to show only results that have available parking for cars

--- src/Park_and_ride_api.py
+++ src/Park_and_ride_api.py
@@ -1,6 +1,6 @@
 import requests
 def park_and_ride_api():
-    response = requests.get('https://p.hsl.fi/api/v1/facilities.json')
+    response = requests.get('https://p.hsl.fi/api/v1/facilities?capacity-types=CAR&statuses=IN_OPERATION')
     if response.status_code == 200:
       return(response.content)
     else:
